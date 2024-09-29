f512e20e0dac99d5f8581f27e1e15b8e2bd14c46
SHADIK <shadik.sk420@gmail.com>
2024-09-30 05:56:31 +0600

    added mouse tracing canvas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
+import MouseTracingCanvas from './canvas/MouseTracingCanvas';
 
 const Hero = () => {
   return (
@@ -26,6 +27,7 @@ const Hero = () => {
       </div>
 
       <ComputersCanvas />
+      <MouseTracingCanvas/>
 
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
