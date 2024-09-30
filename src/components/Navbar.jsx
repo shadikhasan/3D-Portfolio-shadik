b9ae964ab41b3b6bac843c74bf2b9c5f89d3fd04
SHADIK <shadik.sk420@gmail.com>
2024-10-01 03:58:35 +0600

    navbar ui updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,20 +49,27 @@ const Navbar = () => {
           </p>
         </Link>
 
+        {/* Desktop Menu */}
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((nav) => (
             <li
               key={nav.id}
-              className={`${
+              className={`relative ${
                 active === nav.title ? "text-white" : "text-secondary"
-              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              } hover:text-white text-[18px] font-medium cursor-pointer transition duration-300 ease-in-out`}
               onClick={() => setActive(nav.title)}
             >
               <a href={`#${nav.id}`}>{nav.title}</a>
+              <span
+                className={`absolute bottom-[-4px] left-0 w-full h-[2px] bg-gradient-to-r from-pink-500 to-purple-500 transform scale-x-0 transition-transform duration-300 ease-in-out ${
+                  active === nav.title ? "scale-x-100" : ""
+                } hover:scale-x-100`}
+              ></span>
             </li>
           ))}
         </ul>
 
+        {/* Mobile Menu Toggle */}
         <div className='sm:hidden flex flex-1 justify-end items-center'>
           <img
             src={toggle ? close : menu}
@@ -74,21 +81,26 @@ const Navbar = () => {
           <div
             className={`${
               !toggle ? "hidden" : "flex"
-            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+            } p-6 bg-black bg-opacity-80 absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
           >
             <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
               {navLinks.map((nav) => (
                 <li
                   key={nav.id}
-                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                  className={`relative font-poppins font-medium cursor-pointer text-[16px] ${
                     active === nav.title ? "text-white" : "text-secondary"
-                  }`}
+                  } hover:text-white transition duration-300 ease-in-out`}
                   onClick={() => {
                     setToggle(!toggle);
                     setActive(nav.title);
                   }}
                 >
                   <a href={`#${nav.id}`}>{nav.title}</a>
+                  <span
+                    className={`absolute bottom-[-4px] left-0 w-full h-[2px] bg-gradient-to-r from-pink-500 to-purple-500 transform scale-x-0 transition-transform duration-300 ease-in-out ${
+                      active === nav.title ? "scale-x-100" : ""
+                    } hover:scale-x-100`}
+                  ></span>
                 </li>
               ))}
             </ul>
